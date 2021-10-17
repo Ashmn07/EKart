99f7ec220081b7de0b1b8ce173650660e200e4bc
Ashmn07 <ashwin.menon202@gmail.com>
2021-10-17 11:29:07 +0530

    Added Cart Register and Login UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import { Home } from './pages/Home';
+import { Product } from './pages/Product';
+import { ProductList } from './pages/ProductList';
+import Register from './pages/Login';
+import Cart from './pages/Cart';
 
 function App(){
   return (
-    <Home/>
+    <Cart/>
   );
 }
 
